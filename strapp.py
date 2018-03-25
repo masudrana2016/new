4ba155210e02f6fe17a2f142cacaaa26d4868821
masudrana2016 <mrmr5669@gmail.com>
2018-03-25 13:29:20 +0600

    new

---
+++ strapp.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+import os, io, sys, time, re, csv
+from selenium import webdriver 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
+import threading
+from datetime import datetime
+import datetime
+import random
+sk="proxy.txt"
+fky="keyword.txt"
+if len(sk)<=2 and len(fky)<=2:
+        print("please set path proxy ")
+else:
+    with open(fky, 'r+') as fkky:
+        for row in fkky:
+            print(" Start Extracting All questions of : " +str(row)) 
+            data=[]
+            with open (sk, 'r+') as fils:
+                for rr in fils:
+                      data.append(rr)
+            prs=random.choice(data)
+            options = webdriver.ChromeOptions()
+            options.add_argument("--headless")
+            options.add_argument("--proxy-server=socks5://"+str(prs));
+            options.add_argument("--window-size=1024,768")
+            driver = webdriver.Chrome(chrome_options=options)
+            driver.get("https://www.quora.com/search?q="+str(row))
+            print(prs)
+            for x in range(0,8):
+                time.sleep(1)
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            data = driver.page_source
+            shoup=BeautifulSoup(data, "lxml")
+
+            rsk=shoup.findAll('a', attrs={'class': 'question_link'})
+            links=[]
+            for a in rsk:
+                 try:
+                    links.append(a['href'])
+                 except KeyError:
+                    pass                   
+            print(links)
+            par = "output.txt"
+            with open(par, 'a+') as file :
+                for raw in links:
+                    rk=raw.replace("-"," ").replace("/","")
+                    rd="https://www.quora.com"+raw
+                    file.writelines("url:  "+rd +'\n')
+                    file.writelines("Questions:  "+rk+"?"+ '\n')
+                    file.writelines("------"+'\n')
+            driver.close()
+            print(" All questions of : " +str(row)+"   Successfully Done")
+
+
+
+
+
+
+            
+
